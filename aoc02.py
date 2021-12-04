4bb7df5927d3bf4def206facf9c249dd993df719
Brad Eisenhauer <bradley.eisenhauer@climate.com>
2021-12-04 09:07:47 -0600

    Add tests for Day 2

--- aoc02.py
+++ aoc02.py
@@ -1,23 +1,25 @@
-"""Calculate the final position"""
-
+"""AoC 2021, Day 02: https://adventofcode.com/2021/day/2"""
 from functools import reduce
+from io import StringIO
 from pathlib import Path
+from typing import NewType
 
 import numpy as np
 
-INPUT_FILE = Path(__file__).parent / "resources" / "input02.txt"
+from util import get_input_path
 
-
-Movement = np.ndarray  # Movement vector consists of displacement, ∆ aim
-State = np.ndarray  # State vector consists of x, y, aim
+Movement = NewType(
+    "Movement", np.ndarray  # Movement vector consists of displacement, ∆ aim
+)
+State = NewType("State", np.ndarray)  # State vector consists of x, y, aim
 
 
 def create_movement(displacement, delta_aim) -> Movement:
-    return np.array([[displacement, delta_aim]])
+    return Movement(np.array([[displacement, delta_aim]]))
 
 
 def create_state(x=0, y=0, aim=0) -> State:
-    return np.array([[x, y, aim]])
+    return State(np.array([[x, y, aim]]))
 
 
 UNIT_MOVEMENTS = {
@@ -27,15 +29,20 @@ UNIT_MOVEMENTS = {
 }
 
 
-def main():
-    with open(INPUT_FILE) as fp:
-        movements = (translate_instruction(instruction) for instruction in fp)
-        initial_state = create_state()
-        final_state = reduce(move, movements, initial_state)
+def main(input_path: Path):
+    with open(input_path) as fp:
+        final_state = run_simulation(fp)
 
     print(final_state)
     x, depth, _ = final_state[0]
     print(x * depth)
+
+
+def run_simulation(input_stream):
+    movements = (translate_instruction(instruction) for instruction in input_stream)
+    initial_state = create_state()
+    final_state = reduce(move, movements, initial_state)
+    return final_state
 
 
 def translate_instruction(instruction: str) -> Movement:
@@ -58,5 +65,23 @@ def move(initial: State, movement: Movement) -> State:
     return initial + movement.dot(transform)
 
 
+TEST_INPUT = """forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+"""
+
+
+def test_main():
+    from numpy.testing import assert_array_equal
+
+    with StringIO(TEST_INPUT) as fp:
+        final_state = run_simulation(fp)
+
+    assert_array_equal(create_state(15, 60, 10), final_state)
+
+
 if __name__ == "__main__":
-    main()
+    main(get_input_path(2))
