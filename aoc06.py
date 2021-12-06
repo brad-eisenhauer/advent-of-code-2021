706373cea25579300fa8da6039b3a4183815ac44
Brad Eisenhauer <bradley.eisenhauer@climate.com>
2021-12-06 14:33:43 -0600

    Add Fibnacci-like solution to D6

--- aoc06.py
+++ aoc06.py
@@ -1,7 +1,7 @@
 """Advent of Code 2021, Day 06: https://adventofcode.com/2021/day/6"""
 from functools import lru_cache
 from io import StringIO
-from typing import Iterable, Iterator, TextIO, Union
+from typing import Iterable, Iterator, List, TextIO
 
 import pytest
 
@@ -40,6 +40,38 @@ def count_descendants(days_until_spawn: int, simulation_length: int) -> int:
     return count_descendants(0, simulation_length - days_until_spawn)
 
 
+"""This represents the daily population from a single fish, spawned ex nihilo"""
+POPULATION_BY_DAY = (
+    (1,) * MATURITY_INTERVAL
+    + (2,) * SPAWN_INTERVAL
+    + (3,) * (MATURITY_INTERVAL - SPAWN_INTERVAL)
+)
+
+
+def calc_population_by_day(
+    days_until_spawn: Iterable[int], simulation_length: int
+) -> List[int]:
+    """
+    The population on any given day is equal to the sum of the populations 7 and 9
+    days prior.  Inspired by https://wild.maths.org/fibonacci-and-bees
+    Unfortunately, it's still slower that the recursive solution above.
+    """
+    population_by_day = [
+        sum(populations)
+        for populations in zip(
+            *(
+                POPULATION_BY_DAY[MATURITY_INTERVAL - d : 2 * MATURITY_INTERVAL - d]
+                for d in days_until_spawn
+            )
+        )
+    ]
+    for _ in range(simulation_length - MATURITY_INTERVAL):
+        population_by_day.append(
+            population_by_day[-MATURITY_INTERVAL] + population_by_day[-SPAWN_INTERVAL]
+        )
+    return population_by_day
+
+
 TEST_INPUT = "3,4,3,1,2"
 
 
@@ -49,7 +81,16 @@ def test_count_descendants(sim_days, expected):
         initial_states = read_states(fp)
 
     result = count_all_descendants(initial_states, simulation_length=sim_days)
-    assert expected == result
+    assert result == expected
+
+
+@pytest.mark.parametrize(("sim_days", "expected"), ((80, 5934), (256, 26984457539)))
+def test_calc_population_by_day(sim_days, expected):
+    with StringIO(TEST_INPUT) as fp:
+        initial_states = read_states(fp)
+
+    result = calc_population_by_day(initial_states, sim_days)
+    assert result[-1] == expected
 
 
 if __name__ == "__main__":
