421a151c55d709b21d7654b97a3e4f6db75bc011
Brad Eisenhauer <bradley.eisenhauer@climate.com>
2021-12-10 13:03:09 -0600

    Even more tinkering with Day; finally like this one

--- aoc2021/aoc09.py
+++ aoc2021/aoc09.py
@@ -2,8 +2,9 @@
 from __future__ import annotations
 
 import operator
+from dataclasses import dataclass
 from enum import Enum
-from functools import cached_property, lru_cache, reduce
+from functools import cached_property, reduce
 from io import StringIO
 from itertools import product
 from pathlib import Path
@@ -12,6 +13,14 @@ from typing import Iterable, Iterator, Optional, TextIO
 import pytest
 
 from util import get_input_path, timer
+
+Point = tuple[int, int]
+
+
+@dataclass
+class Basin:
+    members: set[Point]
+    low: Point
 
 
 class Direction(tuple, Enum):
@@ -29,7 +38,7 @@ class Direction(tuple, Enum):
             self.RIGHT: self.LEFT,
         }[self]
 
-    def from_point(self, point: tuple[int, int]) -> tuple[int, int]:
+    def from_point(self, point: Point) -> Point:
         row, col = point
         row_offset, col_offset = self.value
         return row + row_offset, col + col_offset
@@ -37,33 +46,41 @@ class Direction(tuple, Enum):
 
 class HeightMap:
     def __init__(self, heights: Iterable[Iterable[int]]):
-        self.heights = tuple((9, *row, 9) for row in heights)
-        map_width = len(self.heights[0])
-        self.heights = ((9,) * map_width, *self.heights, (9,) * map_width)
+        # pad heights with border of 9s
+        heights = tuple((9, *row, 9) for row in heights)
+        map_width = len(heights[0])
+        self.heights = ((9,) * map_width, *heights, (9,) * map_width)
 
     @classmethod
     def read(cls, fp: TextIO) -> HeightMap:
         return cls((int(n) for n in line.strip()) for line in fp)
 
-    def get_height(self, point: tuple[int, int]) -> int:
+    def get_height(self, point: Point) -> int:
         row, col = point
         return self.heights[row][col]
 
-    def find_low_points(self) -> Iterator[tuple[int, int]]:
+    @cached_property
+    def basins(self) -> list[Basin]:
+        result = []
+
         row_range = range(1, len(self.heights) - 1)
         col_range = range(1, len(self.heights[0]) - 1)
 
-        found_low_point = [[False] * len(row) for row in self.heights]
+        all_basin_points = set()
 
         for point in product(row_range, col_range):
-            if found_low_point[point[0]][point[1]] or self.get_height(point) == 9:
+            if point in all_basin_points or self.get_height(point) == 9:
                 continue
             if (low_point := self.find_low_point(point)) is not None:
-                yield low_point
-                for row, col in self.find_basin_points(low_point):
-                    found_low_point[row][col] = True
+                basin = Basin(
+                    members=set(self.find_basin_points(low_point)), low=low_point
+                )
+                result.append(basin)
+                all_basin_points |= basin.members
 
-    def find_low_point(self, start: tuple[int, int]) -> Optional[tuple[int, int]]:
+        return result
+
+    def find_low_point(self, start: Point) -> Optional[Point]:
         value = self.get_height(start)
         if value == 9:
             return None
@@ -79,48 +96,8 @@ class HeightMap:
         except StopIteration:
             return start
 
-    def find_upstream_points(
-        self, start: tuple[int, int], acc: Optional[set[int]] = None
-    ) -> set[tuple[int, int]]:
-        """
-        Start with the current point in the accumulator.
-        For each surrounding point:
-            - If the point is already in the accumulator, skip it.
-            - If any flows from the adjacent point flow back to the current point, add
-                the adjacent point to the accumulator, and call find_basin_points with
-                the adjacent point as the starting point.
-        """
-        if acc is None:
-            acc = {start}
-
-        value = self.get_height(start)
-        for direction in Direction:
-            adj_point = direction.from_point(start)
-            if adj_point in acc:
-                continue
-            adj_value = self.get_height(adj_point)
-            if value < adj_value < 9:
-                acc = self.find_upstream_points(adj_point, acc | {adj_point})
-
-        return acc
-
-    def find_basin_sizes(self) -> Iterator[int]:
-        map_height = len(self.heights)
-        map_width = len(self.heights[0])
-
-        is_basin = list([False] * map_width for _ in range(map_height))
-
-        for point in product(range(map_height), range(map_width)):
-            row, col = point
-            if not is_basin[row][col] and self.get_height(point) < 9:
-                basin_size = 0
-                for row, col in self.find_basin_points(point):
-                    basin_size += 1
-                    is_basin[row][col] = True
-                yield basin_size
-
-    def find_basin_points(self, start: tuple[int, int]) -> Iterator[tuple[int, int]]:
-        """Non-recursive BFS alternative to find_upstream_points"""
+    def find_basin_points(self, start: Point) -> Iterator[Point]:
+        """ BFS for all points until we hit the wall of 9s """
         queue = [start]
         visited = set()
 
@@ -137,7 +114,14 @@ def main(input_path: Path):
     with open(input_path) as fp:
         height_map = HeightMap.read(fp)
 
-    ranked_basin_sizes = sorted(height_map.find_basin_sizes(), reverse=True)
+    # Part 1 - calc total risk score
+    score = sum(height_map.get_height(basin.low) + 1 for basin in height_map.basins)
+    print(score)
+
+    # Part 2 - product of sizes of 3 largest basins
+    ranked_basin_sizes = sorted(
+        (len(basin.members) for basin in height_map.basins), reverse=True
+    )
     result = reduce(operator.mul, ranked_basin_sizes[:3])
 
     print(result)
@@ -159,18 +143,14 @@ def height_map():
 
 
 def test_calc_sum_of_risks(height_map):
-    low_points = height_map.find_low_points()
-    result = sum(height_map.get_height(p) + 1 for p in low_points)
+    result = sum(height_map.get_height(basin.low) + 1 for basin in height_map.basins)
     assert result == 15
 
 
 def test_sum_3_largest_basins(height_map):
-    # low_points = height_map.find_low_points()
-    # basin_sizes = (
-    #     sum(1 for _ in height_map.find_basin_points(low_point))
-    #     for low_point in low_points
-    # )
-    ranked_basin_sizes = sorted(height_map.find_basin_sizes(), reverse=True)
+    ranked_basin_sizes = sorted(
+        (len(basin.members) for basin in height_map.basins), reverse=True
+    )
     result = reduce(operator.mul, ranked_basin_sizes[:3])
 
     assert result == 1134
