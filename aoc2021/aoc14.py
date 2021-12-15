59a4a5a4e7fcb90070243b162bdfcf501533105d
Brad Eisenhauer <bradley.eisenhauer@gmail.com>
2021-12-15 12:18:57 -0600

    Add Formula class

--- aoc2021/aoc14.py
+++ aoc2021/aoc14.py
@@ -1,59 +1,68 @@
 """ Advent of Code 2021, Day 14: https://adventofcode.com/2021/day/14 """
+from __future__ import annotations
 
+import unittest
 from collections import Counter
 from functools import cache
 from io import StringIO
 from pathlib import Path
-from typing import Iterator, TextIO
-
-import pytest
+from typing import Iterable, TextIO
 
 from util import get_input_path, timer
-
-Formula = dict[str, str]
 
 
 def main(input_path: Path):
     with open(input_path) as fp:
         template, formula = read_input(fp)
 
-    counts = count_elements(template, formula, 40)
-    diff = max(counts.values()) - min(counts.values())
-    print(diff)
+    result = formula.calc_element_count_range(template, 40)
+    print(result)
 
 
 def read_input(fp: TextIO) -> tuple[str, Formula]:
     template = next(fp).strip()
     _ = next(fp)  # throw away empty line
-    formula = {k: v for k, _, v in (line.strip().split() for line in fp)}
+    formula = Formula.read(fp)
     return template, formula
 
 
-def count_elements(template: str, formula: Formula, steps: int) -> Counter:
-    @cache
-    def count_elements_rec(partial_template: str, steps: int) -> Counter:
-        if steps == 0:
-            return Counter(partial_template)
+class Formula:
+    def __init__(self, rules: Iterable[str]):
+        self.rules = {k: v for k, _, v in (line.strip().split() for line in rules)}
 
-        if len(partial_template) > 2:
-            # split string and count each half separately
-            mid_idx = len(partial_template) // 2
-            left = partial_template[:mid_idx + 1]
-            right = partial_template[mid_idx:]
-            result = count_elements_rec(left, steps) + count_elements_rec(right, steps)
+    @classmethod
+    def read(cls, fp: TextIO) -> Formula:
+        return cls(fp)
+
+    @cache
+    def count_elements(self, template: str, steps: int) -> Counter:
+        if steps == 0:
+            return Counter(template)
+
+        if len(template) > 2:
+            # split string and count each part separately
+            mid_idx = len(template) // 2
+            left = template[: mid_idx + 1]
+            right = template[mid_idx:]
+            result = self.count_elements(left, steps) + self.count_elements(
+                right, steps
+            )
             # middle character is included in both counts
-            result[partial_template[mid_idx]] -= 1
+            result[template[mid_idx]] -= 1
             return result
 
-        new_char = formula[partial_template]
-        result = (
-            count_elements_rec(partial_template[0] + new_char, steps - 1)
-            + count_elements_rec(new_char + partial_template[1], steps - 1)
-        )
+        new_char = self.rules[template]
+        result = self.count_elements(
+            template[0] + new_char, steps - 1
+        ) + self.count_elements(new_char + template[1], steps - 1)
         result[new_char] -= 1
         return result
 
-    return count_elements_rec(template, steps)
+    def calc_element_count_range(self, template: str, steps: int) -> int:
+        counts = self.count_elements(template, steps)
+        min_count = min(counts.values())
+        max_count = max(counts.values())
+        return max_count - min_count
 
 
 SAMPLE_INPUT = """\
@@ -78,19 +87,23 @@ CN -> C
 """
 
 
-@pytest.fixture
-def sample_input() -> Iterator[TextIO]:
-    with StringIO(SAMPLE_INPUT) as fp:
-        yield fp
-
-
-@pytest.mark.parametrize(("step_count", "expected"), ((10, 1588), (40, 2188189693529)))
-def test_most_least_common_elements(sample_input, step_count, expected):
-    template, formula = read_input(sample_input)
-    counts = count_elements(template, formula, step_count)
-    min_count = min(counts.values())
-    max_count = max(counts.values())
-    assert max_count - min_count == expected
+class ElementCountTests(unittest.TestCase):
+    def test_calc_element_count_range(self):
+        for source, steps, expected in (
+            (SAMPLE_INPUT, 10, 1588),
+            (SAMPLE_INPUT, 40, 2188189693529),
+            (None, 10, 3906),
+            (None, 40, 4441317262452),
+        ):
+            with self.subTest(source=source, steps=steps, expected=expected):
+                with (
+                    StringIO(source)
+                    if source is not None
+                    else open(get_input_path(14, 2021))
+                ) as fp:
+                    template, formula = read_input(fp)
+                result = formula.calc_element_count_range(template, steps)
+                self.assertEqual(expected, result)
 
 
 if __name__ == "__main__":
