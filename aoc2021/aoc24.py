2c854a36fbde0e6b2f9ea275ef359917a7109fd6
Brad Eisenhauer <bradley.eisenhauer@gmail.com>
2021-12-25 08:00:17 -0600

    Well, it works.

--- aoc2021/aoc24.py
+++ aoc2021/aoc24.py
@@ -1,9 +1,9 @@
 """ Advent of Code 2021, Day 24: https://adventofcode.com/2021/day/24 """
-from enum import Enum
+import heapq
 from io import StringIO
-from itertools import islice, product
+from itertools import islice
 from pathlib import Path
-from typing import Iterable, Iterator, Sequence, TextIO
+from typing import Iterable, Iterator, Sequence, TextIO, Optional
 
 import pytest
 
@@ -20,29 +20,79 @@ def main(input_path: Path, timer: Timer):
     timer.check("Part 1")
 
 
+History = tuple[int, ...]
+Program = Sequence[str]
+ProgramIndex = int
 Register = tuple[int, ...]
+Result = int
+State = tuple[ProgramIndex, Result]
 
 
-def find_largest_valid_number(monad: Sequence[str]) -> int:
-    digit_range = range(9, 0, -1)
-    for model_number in product(*((digit_range,) * 14)):
-        if not run_program(monad, model_number)[-1]:
-            break
-    result = 0
-    for d in model_number:
-        result *= 10
-        result += d
+def read_program(input_path: Path) -> Program:
+    with open(input_path) as fp:
+        return list(fp)
 
-    return result
+
+def find_largest_valid_number(monad: Program) -> Optional[int]:
+    sub_progs = divide_program(monad)
+    greatest_values: dict[State, History] = {(0, 0): ()}
+    states = [(0, 0)]
+    goal_state = (len(sub_progs), 0)
+
+    state_count = 0
+    while len(states) > 0:
+        state = states.pop()
+        state_count += 1
+
+        if state_count % 1000 == 0:
+            print(f"{state_count} states processed ({len(state)} in stack); current {state}")
+
+        prog_index, z_value = state
+        if prog_index not in range(len(sub_progs)):
+            continue
+
+        for n in range(9, 0, -1):
+            prog_result = run_program(sub_progs[prog_index], [n], initial_register=(0, 0, 0, z_value))
+            new_state = (prog_index + 1, prog_result[-1])
+            history = (*greatest_values[state], n)
+
+            if new_state not in greatest_values:
+                states.append(new_state)
+                greatest_values[new_state] = history
+            elif greatest_values[new_state] < history:
+                greatest_values[new_state] = history
+
+            if new_state == goal_state:
+                result = 0
+                for d in greatest_values[goal_state]:
+                    result *= 10
+                    result += d
+                return result
+
+    return None
+
+
+def divide_program(program: Program) -> Sequence[Program]:
+    result = []
+    next_sub_prog = []
+    for line in program:
+        if line.startswith("inp"):
+            result.append(next_sub_prog)
+            next_sub_prog = [line]
+        else:
+            next_sub_prog.append(line)
+    result.append(next_sub_prog)
+    return result[1:]
 
 
 def run_program(
         program: Iterable[str],
         stdin: Iterable[int],
         start_line: int = 0,
+        stop_line: Optional[int] = None,
         initial_register: Register = (0, 0, 0, 0)
 ) -> Register:
-    program = islice(program, start_line)
+    program = islice(program, start_line, stop_line)
     stdin = iter(stdin)
     register = list(initial_register)
 
